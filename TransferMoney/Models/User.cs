4aa7c59a961d70e9e6ce41999955c434d8631b44
Bayr_kf60 <Bayr_kf60@STHQ012D-08>
2022-12-25 17:36:28 +0400

    Add project files.

---
+++ TransferMoney/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferMoney.Models
+{
+    public class User
+    {
+        public string CardNumber { get; set; }
+        public string FullName { get; set; }
+        public double Balance { get; set; }
+
+    }
+}
---
+++ TransferMoney/ViewModels/MainViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TransferMoney.Commands;
+using TransferMoney.Models;
+
+namespace TransferMoney.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string cardNumber;
+
+        public string CardNumber
+        {
+            get { return cardNumber; }
+            set { cardNumber = value; }
+        }
+
+        public List<User> Users { get; set; }
+
+
+        private User user;
+
+        public User User
+        {
+            get { return user; }
+            set { user = value;OnPropertyChanged(); }
+        }
+
+        public RelayCommand LoadDataCommand { get; set; }
+        public MainViewModel()
+        {
+            User = new User();
+            Users = new List<User>()
+            {
+                new User
+                {
+                     CardNumber="4098584465729489",
+                     FullName="Mehemmed Bayramov",
+                     Balance=4321.43
+                },
+                new User
+                {
+                     CardNumber="1122334455667788",
+                     Balance=2233.12,
+                     FullName="Remzi Hesenov"
+                },
+                new User
+                {
+                    CardNumber="1111222233334444",
+                    FullName="Nurlan Shirinov",
+                    Balance=4467.36
+                }
+            };
+
+
+            LoadDataCommand = new RelayCommand((o) =>
+            {
+                User = Users.FirstOrDefault((u) => { return u.CardNumber == CardNumber; });
+                if (User == null) MessageBox.Show("User not found");
+            });
+
+
+        }
+    }
+}
